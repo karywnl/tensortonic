c48cc9bbec3f6ce19d345ba660a24ba45520acb3
kary <119475071+karywnl@users.noreply.github.com>
2026-07-21 14:19:06 +0530

    Solve numpy-row-scaling

--- study-plans/numpy-basics/numpy-row-scaling/numpy-row-scaling.py
+++ study-plans/numpy-basics/numpy-row-scaling/numpy-row-scaling.py
@@ -3,8 +3,6 @@ import numpy as np
 def scale_rows(data, weights):
     """Returns: np.ndarray of shape (m, n), each row scaled by corresponding weight"""
     arr = np.array(data, dtype='float64')
+    scalars = np.array(weights, dtype='float64')
 
-    for i in range(len(weights)):
-        arr[i] = weights[i] * arr[i]
-
-    return arr
+    return arr * scalars[:, np.newaxis]
