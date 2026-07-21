6e33ae5dacebd9d09aab453c4f243b0e10e906c3
kary <119475071+karywnl@users.noreply.github.com>
2026-07-21 14:28:13 +0530

    Solve numpy-pairwise-diff

--- study-plans/numpy-basics/numpy-pairwise-diff/numpy-pairwise-diff.py
+++ study-plans/numpy-basics/numpy-pairwise-diff/numpy-pairwise-diff.py
@@ -4,11 +4,5 @@ def pairwise_diff(a):
     """Returns: np.ndarray of shape (n, n) where out[i,j] = a[i] - a[j]"""
 
     arr = np.array(a, dtype=np.float64)
-    n = len(arr)
-    res = np.zeros((n, n))
-    
-    for i in range(n):
-        for j in range(n):
-            res[i, j] = arr[i] - arr[j]
 
-    return res
+    return arr[:, np.newaxis] - arr[np.newaxis, :]
