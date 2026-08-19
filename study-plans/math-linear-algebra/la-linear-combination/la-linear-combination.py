74f6e7a2edc4e02df38d8210176572c7db910252
kary <119475071+karywnl@users.noreply.github.com>
2026-08-19 23:48:05 +0530

    Solve la-linear-combination

---
+++ study-plans/math-linear-algebra/la-linear-combination/la-linear-combination.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def linear_combination(vectors, coefficients):
+    """
+    Returns: float64 array, the weighted sum of vectors.
+    """
+    vectors = np.array(vectors)
+    coefficients = np.array(coefficients)
+
+    res = np.zeros(len(vectors[0]))
+
+    for i in range(len(vectors)):
+        res += coefficients[i] * vectors[i]
+
+    return res
